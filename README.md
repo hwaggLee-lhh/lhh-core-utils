871fd02812f6dfc52d862c6bd0615fa34325f821
lihonghua <243573259@qq.com>
2017-02-24 16:16:39 +0800

    未做修改

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # lhh-core-utils
 各种统一工具封装使用
+
+detector:探测器
---
+++ src/main/java/com/lhh/detector/LhhUtilsFileDetector.java
@@ -0,0 +1,13 @@
+package com.lhh.detector;
+
+/**
+ * 文件探测器
+ * @author hwaggLee
+ * @createDate 2017年2月24日
+ */
+
+/**
+ * 资源变化自动检测
+ * @author 杨尚川
+ */
+public class LhhUtilsFileDetector {}
